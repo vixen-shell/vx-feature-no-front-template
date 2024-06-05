06b44be983c34e70da7847b012e266f4b1a79d28
Nohavye <noha.poncelet@gmail.com>
2024-06-05 15:37:46 +0200

    chore: Update default config

--- config/root/feature/__init__.py
+++ config/root/feature/__init__.py
@@ -1,7 +1,7 @@
 from vx_feature_utils import Utils
 
 utils = Utils.define_feature_utils()
-content = Utils.define_feature_content()
+content = Utils.define_feature_content({"frames": "disable", "state": "disable"})
 
 
 @content.on_startup
