02869146518b19fa141859962b19cd4288133c08
Nohavye <noha.poncelet@gmail.com>
2024-10-25 14:54:59 +0200

    chore: Update feature template

--- root/feature/__init__.py
+++ root/feature/__init__.py
@@ -1,14 +1,21 @@
-from vx_root import root_feature, utils
+from vx_root import root_feature, show_message_dialog
 
 feature = root_feature()
-feature.init({"frames": "disable", "state": "disable"})
+feature.init(
+    {
+        "title": "Creating a new feature for Vixen Shell",
+        "frames": "disable",
+    }
+)
 
 
 @feature.on_startup
 def on_startup():
-    utils.dialog(
+    show_message_dialog(
         "You have just created a new feature without a front-end for Vixen Shell.\n"
-        "All you have to do now is implement the different contents of this feature.\n"
+        "All you have to do now is implement the different contents of this feature.\n\n"
         "Happy developing!",
-        title=f"Feature: {feature.name}",
+        title=feature.params.get_value("title"),
     )
+
+    return True
