6544812a102f1541eb1ad5fbb368ab3c3e577801
Nohavye <noha.poncelet@gmail.com>
2024-11-15 15:46:47 +0100

    chore: update

--- root/feature/__init__.py
+++ root/feature/__init__.py
@@ -1,21 +1,21 @@
 from vx_root import root_feature, show_message_dialog
 
 feature = root_feature()
+
+
+def feature_life_cycle():
+    show_message_dialog(
+        title=feature.params.get_value("title"),
+        text="You have just created a new feature without a front-end for Vixen Shell.\n"
+        "All you have to do now is implement the different contents of this feature.\n\n"
+        "Happy developing!",
+    )
+
+
 feature.init(
     {
         "title": "Creating a new feature for Vixen Shell",
         "frames": "disable",
+        "life_cycle": feature_life_cycle,
     }
 )
-
-
-@feature.on_startup
-def on_startup():
-    show_message_dialog(
-        "You have just created a new feature without a front-end for Vixen Shell.\n"
-        "All you have to do now is implement the different contents of this feature.\n\n"
-        "Happy developing!",
-        title=feature.params.get_value("title"),
-    )
-
-    return True
